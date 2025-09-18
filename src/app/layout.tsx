c62c649ec0ac52ea5bc314331c652a8185eb88bb
Vineet Raj <vineetcres@gmail.com>
2025-09-19 01:09:27 +0530

    well deserved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "portfolio.exe",
+  title: "Vineet Raj",
   description: "author: vineet raj",
 };
 
@@ -14,12 +14,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
-        <link href="https://api.fontshare.com/v2/css?f[]=tabular@400,500,700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
       </head>
       <body
-        className="font-[Tabular,monospace] antialiased"
+        style={{ fontFamily: 'Inter, sans-serif' }}
+        className="antialiased"
       >
         {children}
       </body>
